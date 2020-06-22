00a1615326f8149c00e603b0379a4ab474615262
chengbotao <t221007s@163.com>
2020-06-22 13:10:43 +0800

    feat(src/index.ts): The processing of url
    The processing of the url parameter

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,28 @@
  * @Author: Chengbotao
  * @Date: 2020-06-22 05:49:10
  * @LastEditors: Chengbotao
- * @LastEditTime: 2020-06-22 06:15:24
+ * @LastEditTime: 2020-06-22 13:07:49
  */
 
 import { AxiosRequestConfig } from './types/index'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
   // TODO
+  processConfig(config)
   xhr(config)
 }
+
+// 处理 config
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+// 处理 config.url
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
 export default axios
