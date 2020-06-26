80a5b45817c0b204345fb1f0e25b9bc5624ca912
chengbotao <t221007s@163.com>
2020-06-26 09:54:22 +0800

    test(config): 添加请求响应配置化 demo
    添加请求响应配置化 demo

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,16 +1,23 @@
 /*
- * @Description: config
- * @version:
- * @Author: Chengbotao
- * @Date: 2020-06-25 18:03:27
+* @Description: config
+* @version:
+* @Author: Chengbotao
+* @Date: 2020-06-25 18:03:27
  * @LastEditors: Chengbotao
- * @LastEditTime: 2020-06-25 18:11:50
- */
+ * @LastEditTime: 2020-06-26 09:52:51
+*/
 import axios from "../../src/index"
+import { AxiosTransformer } from './../../src/types/index';
 
 axios.defaults.headers.common["test2"] = 123
 // tslint:disable-next-line: no-floating-promises
 axios({
+  transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function (data) {
+    if (typeof data === "object") {
+      data.b = 2
+    }
+    return data
+  }],
   url: "/config/post",
   method: "post",
   headers: {
