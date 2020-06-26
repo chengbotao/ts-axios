e0aad9a88d3a2a4e78106fb9a6f9ea54d56b1cea
chengbotao <t221007s@163.com>
2020-06-26 10:12:34 +0800

    test(config): create 静态方法的 demo
    create 静态方法的 demo

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -4,20 +4,35 @@
 * @Author: Chengbotao
 * @Date: 2020-06-25 18:03:27
  * @LastEditors: Chengbotao
- * @LastEditTime: 2020-06-26 09:52:51
+ * @LastEditTime: 2020-06-26 10:10:06
 */
 import axios from "../../src/index"
 import { AxiosTransformer } from './../../src/types/index';
 
 axios.defaults.headers.common["test2"] = 123
-// tslint:disable-next-line: no-floating-promises
-axios({
+const Instance = axios.create({
   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function (data) {
     if (typeof data === "object") {
       data.b = 2
     }
     return data
-  }],
+  }]
+})
+// tslint:disable-next-line: no-floating-promises
+Instance({
+  url: "/config/post",
+  method: "post",
+  headers: {
+    test: "321"
+  },
+  data: {
+    a: "1"
+  }
+}).then(res => {
+  console.log(res)
+})
+// tslint:disable-next-line: no-floating-promises
+axios({
   url: "/config/post",
   method: "post",
   headers: {
